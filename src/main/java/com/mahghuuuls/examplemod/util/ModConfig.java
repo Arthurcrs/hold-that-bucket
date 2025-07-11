66def72e393ea89f58161f67369c82586e88eae4
Mahghuuuls <arthcrs@gmail.com>
2025-07-10 22:04:14 -0300

    Added drop logic.. it is not working yet

--- src/main/java/com/mahghuuuls/examplemod/util/ModConfig.java
+++ src/main/java/com/mahghuuuls/examplemod/util/ModConfig.java
@@ -1,12 +1,15 @@
 package com.mahghuuuls.examplemod.util;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Configuration;
 
 @Config(modid = ModReference.MOD_ID)
 public final class ModConfig {
 
-//	@Config.Comment("Example config")
-//	public static boolean blockMelee = true;
+	private static Configuration cfg;
+
+	@Config.Comment("List of registry names of items that should only be held in the main or off hand")
+	public static String[] restrictedItems = new String[] { "minecraft:diamond_sword" };
 
 	private ModConfig() {
 	}
